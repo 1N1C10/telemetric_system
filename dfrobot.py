40c3a7f466ae4c7e7cc5ab68b7acd114a6d63396
patryk <kr.patryk@wp.pl>
2018-10-30 16:15:08 +0100

    fresh start

--- dfrobot.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-
-
-def modulDFROBOT():
-    df = Pin(2, Pin.IN)
-    if df.value() == 1:
-        return df.value()
-    else:
-        pass
--- esp_proxy/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='esp_proxy_app',
-    packages=['esp_proxy_app'],
-    include_package_data=True,
-    install_requires=['flask']
-)
