a7d8e4193aa15fe600ca2e4ca469f39fae09507d
patryk <kr.patryk@wp.pl>
2018-11-20 01:21:24 +0100

    Proxy app refactor and sending function draft

--- dht22/dht22.py
+++ dht22/dht22.py
@@ -2,9 +2,10 @@ import machine
 import dht
 import time
 
-d = dht.DHT22(machine.Pin(12))
+d = dht.DHT22(machine.Pin(5))
 
 
 def modulDHT22():
     time.sleep(2)
+    d.measure()
     return d.humidity()
